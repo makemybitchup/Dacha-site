c5cc851211b16055b732ebb6c6269a9c451be744
makemybitchup <makestitebicth13@proton.me>
2025-07-14 13:58:33 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,10 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
-      <main className="flex-grow">{children}</main>
+      {children}
       <Footer />
-    </div>
+    </>
   )
 }
-
